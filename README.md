953afb7a2f3b68b96d0cb62f085eea53e0e79bd1
Subrata Sarkar <subrotosarkar32@gmail.com>
2024-03-17 01:36:46 +0530

    root path corection

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # flask-reverse-proxy
+
+Local flask based reverse proxy webapp. The flask app acts as a reverse proxy allowing you to host multiple webapps and redirect based on the url list.
+
+run the application with
+
+`python reverse_proxy.py`
+
+then access some url in any other system's browser to the host system's local ip address lets say you visit http://192.168.1.101
+
+you will get redirected to the locally hosted app lets say http://localhost:5000
+
+if you have pihole setup on your system or network you can configure the dns record and add "A Records" to have your own DNS and access webapps with your own local domain names
+
+
+Lets say on System A you have hosted a webapp locally.
+Now you can bind only one app to port 80.
+But you want to host multiple webapps locally. So whats the solution?
+
+Solution:
+
+You can host your webapps on different ports/sockets(.sock) and use this reverse proxy to serve them on port 80.
+
+First host your webapps locally on different ports , lets say 5000 and 5001.
+
+Then you update the reverse proxy python scripts dictionary to
+
+    "webapp1.local":"http://localhost:5000"
+    or
+    "webapp1.local":"http://unix:/home/subrata32/webapp1/hsecure1.sock"
+
+    "webapp2.local":"http://localhost:5001"
+    or
+    "webapp1.local":"http://unix:/home/subrata32/webapp2/hsecure2.sock"
+
+Then update your pihole dns A Record with
+
+webapp1.local -> 192.168.1.101  # your local ip address
+
+webapp2.local -> 192.168.1.101  # your local ip address
+
--- reverse_proxy.py
+++ reverse_proxy.py
@@ -1,15 +1,39 @@
 from flask import Flask, request, redirect, Response, render_template, abort
 import requests
+import os
 from urllib.parse import urlparse
-app = Flask(__name__)
 
-# SITE_NAME = 'http://localhost:8000'
-SITE_NAME_DICT = {
-                   "manga.private": "http://localhost:5000",  # you can also use sock as "http://unix:/home/subrata32/stream-video-browser/hsecure1.sock"
-                   "example2.com": "http://localhost:8000",  # you can also use sock as "http://unix:/home/subrata32/stream-video-browser/hsecure2.sock"
-                   "example3.com": "http://localhost:9000",  # you can also use sock as "http://unix:/home/subrata32/stream-video-browser/hsecure3.sock"
-                   "192.168.1.101": "http://localhost:10000"  # you can also use sock as "http://unix:/home/subrata32/stream-video-browser/hsecure4.sock"
-}
+
+app = Flask(__name__)
+path_to_file = "URL_DICT.txt"
+SITE_NAME_DICT = {}
+
+
+def process_url_dict(str_text=""):
+    sitey = {}
+    lines = str_text.split("\n")
+    lines = [line for line in lines if (line != "" or line.startswith('#'))]
+    for line in lines:
+        two_part = line.split("->")
+        key = two_part[0].replace('"', '').replace("'", "")
+        value = two_part[1].replace('"', '').replace("'", "")
+        sitey[key] = value
+    return sitey
+
+
+if os.path.exists(path_to_file):
+    file1 = open(path_to_file, "r")
+    file_out = file1.read()
+    file1.close()
+    SITE_NAME_DICT = process_url_dict(str_text=file_out)
+
+if SITE_NAME_DICT == {}:
+    SITE_NAME_DICT = {
+                    "example.private": "http://localhost:5000",  # you can also use sock as "http://unix:/home/subrata32/stream-video-browser/hsecure1.sock"
+                    "example2.com": "http://localhost:8000",  # you can also use sock as "http://unix:/home/subrata32/stream-video-browser/hsecure2.sock"
+                    "example3.com": "http://localhost:9000",  # you can also use sock as "http://unix:/home/subrata32/stream-video-browser/hsecure3.sock"
+                    "192.168.1.101": "http://localhost:10000"  # you can also use sock as "http://unix:/home/subrata32/stream-video-browser/hsecure4.sock"
+    }
 
 
 def get_hostname_str(k=""):
@@ -27,7 +51,36 @@ def index():
     return 'Flask is running!'
 
 
-@app.route('/<path:path>',methods=['GET','POST','DELETE'])
+@app.route('/', methods=['GET', 'POST', 'DELETE'])
+def proxy1():
+    global SITE_NAME_DICT
+    print(SITE_NAME_DICT)
+    SITE_NAMEY = get_hostname_str(k=request.base_url)
+    if SITE_NAMEY in SITE_NAME_DICT:
+        SITE_NAME = SITE_NAME_DICT[SITE_NAMEY]
+        if request.method == 'GET':
+            print(f'{SITE_NAME}/')
+            resp = requests.get(f'{SITE_NAME}/')
+            excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+            headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
+            response = Response(resp.content, resp.status_code, headers)
+            return response
+        elif request.method == 'POST':
+            print(f'{SITE_NAME}/')
+            resp = requests.post(f'{SITE_NAME}/', json=request.get_json())
+            excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+            headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
+            response = Response(resp.content, resp.status_code, headers)
+            return response
+        elif request.method == 'DELETE':
+            resp = requests.delete(f'{SITE_NAME}/').content
+            response = Response(resp.content, resp.status_code, headers)
+            return response
+    else:
+        abort(502)
+
+
+@app.route('/<path:path>', methods=['GET', 'POST', 'DELETE'])
 def proxy(path):
     global SITE_NAME_DICT
     SITE_NAMEY = get_hostname_str(k=request.base_url)
@@ -37,7 +90,7 @@ def proxy(path):
             print(f'{SITE_NAME}/{path}')
             resp = requests.get(f'{SITE_NAME}/{path}')
             excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
-            headers = [(name, value) for (name, value) in  resp.raw.headers.items() if name.lower() not in excluded_headers]
+            headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
             response = Response(resp.content, resp.status_code, headers)
             return response
         elif request.method == 'POST':
